364b27bca4af3a584d6303fd02138407a0161f83
Juan Jesus Balceda Pomachagua <juanbalceda@gmail.com>
2018-10-31 17:26:31 +0100

    Udacity - Project 05 - Movies website

--- capacity/main.py
+++ capacity/main.py
@@ -1,11 +1,17 @@
 def main():
     a, b, c = 3, 2, 1
     if a < b:
-        print("{} es menor que {}".format(a, b))
+        print("{} is less than {}".format(a, b))
     elif c < b:
-        print("{} es menor que {}".format(c, b))
+        print("{} is less than {}".format(c, b))
     else:
-        print("{} es mayor que {}".format(a, b))
+        print("{} is grater than {}".format(a, b))
+
+    days = dict(day1="monday", day2="tuesday", day3="wednesday", day4="thursday", day5="friday", day6="saturday",
+                day7="sunday")
+
+    today = "day5"
+    print(days[today])
 
 
 if __name__ == '__main__':
---
+++ udacity/movies/entertainment_center.py
@@ -0,0 +1,19 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life.",
+                        "https://vignette.wikia.nocookie.net/doblaje/images/3/37/5c1f3c2e3e6dc7ae8d7c99e390162049.jpg/revision/latest?cb=20180423154626&path-prefix=es",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet.",
+                     "https://images-na.ssl-images-amazon.com/images/I/41kTVLeW1CL.jpg",
+                     "https://www.youtube.com/watch?v=6ziBFh3V1aM")
+
+star_wars = media.Movie("Star Wars", "Story of Anakin Skywalker childhood.",
+                        "https://ae01.alicdn.com/kf/HTB12_AsOpXXXXbaaXXXq6xXFXXXs/Star-Wars-episodio-I-La-Amenaza-Fantasma-Movie-posters-impresi-n-tela-de-seda-impresi-n.jpg_640x640.jpg",
+                        "https://www.youtube.com/watch?v=bD7bpG-zDJQ")
+# print(avatar.title)
+# star_wars.show_trailer()
+movies = [toy_story, avatar, star_wars]
+# fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.VALID_RATINGS)
